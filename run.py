0259fc95018b490d9985a53bd656fda4158b85bd
eandablo <>
2023-09-08 12:47:21 +0000

    add funtion to read sales data from user

--- run.py
+++ run.py
@@ -13,6 +13,15 @@ CREDS=Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS=CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT=gspread.authorize(SCOPED_CREDS)
 SHEET=GSPREAD_CLIENT.open('love_sandwiches')
-sales=SHEET.worksheet('sales')
-data=sales.get_all_values()
-print(data)
+
+def get_sales_data():
+    """
+    get sales figures input from user
+    """
+    print('please enter sales data from the last market')
+    print('data should be 6 numbers separated by a coma')
+    print('Example:20,30,41,18,23,32\n')
+    data_str=input('Enter your data here: ')
+    print(f'user data provided is {data_str}')
+
+get_sales_data()
