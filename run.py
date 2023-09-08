e1bd11f396983122a179cd1703c9a041c33a688f
eandablo <>
2023-09-08 10:52:29 +0000

    confrim acces to google sheets

--- run.py
+++ run.py
@@ -10,6 +10,9 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 CREDS=Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS=CREDS.with_scope(SCOPE)
+SCOPED_CREDS=CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT=gspread.authorize(SCOPED_CREDS)
-SHEET=GSPREAD_CLIENT.open('love_sandwishes ')
+SHEET=GSPREAD_CLIENT.open('love_sandwiches')
+sales=SHEET.worksheet('sales')
+data=sales.get_all_values()
+print(data)
